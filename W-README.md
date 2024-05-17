ee9558bc8196ebfbf0fbd05ecad208ea88ccf9e3
desk-white <desk-white@163.com>
2024-05-17 18:19:29 +0800

    本地推理

---
+++ W-README.md
@@ -0,0 +1,18 @@
+# 声音克隆
+项目地址：https://github.com/RVC-Boss/GPT-SoVITS
+
+使用方法：[查看视频](course/使用方法.mp4)
+
+官方文档地址：https://www.yuque.com/baicaigongchang1145haoyuangong/ib3g1e
+
+整合包0217：https://www.icloud.com.cn/iclouddrive/061bfkcVJcBfsMfLF5R2XKdTQ#GPT-SoVITS-beta0217
+ 
+整合包0306fix2：https://www.icloud.com.cn/iclouddrive/09aaTLf96aa92dbLe0fPNM5CQ#GPT-SoVITS-beta0306fix2
+
+安装依赖需要c++编译环境：https://visualstudio.microsoft.com/zh-hans/visual-cpp-build-tools/
+
+# 依赖模型
+
+达摩院语音团队中文标点模型：https://github.com/alibaba-damo-academy/FunASR
+
+FRCRN语音降噪模型是基于频率循环 CRN (FRCRN) 新框架开发出来的
---
+++ white_utils/cover_radio.py
@@ -0,0 +1,39 @@
+# **************************************
+# --*-- coding: utf-8 --*--
+# @Time    : 2024-05-16
+# @Author  : white
+# @FileName: cover_radio.py
+# @Software: PyCharm
+# **************************************
+from pydub import AudioSegment
+import wave
+
+
+# pip install pydub
+# conda install ffmpeg
+
+class CoverRadio:
+    def __init__(self, source_radio: str):
+        self.source_radio = source_radio
+        self.out_file = self.source_radio.rsplit('.', maxsplit=1)[0]
+
+    def to_wav(self, out_file: str = None):
+        """mp3转为wav格式"""
+        sound = AudioSegment.from_file(self.source_radio)
+
+        out_file = out_file or self.out_file + ".wav"
+        sound.export(out_file, format="wav")
+        print(f"转换完成！转换路径：{out_file}")
+
+    def to_mp3(self, out_file: str = None):
+        """wav转换为mp3格式"""
+        audio = AudioSegment.from_wav(self.source_radio)
+
+        out_file = out_file or self.out_file + ".mp3"
+        audio.export(out_file, format="mp3")
+        print(f"转换完成！转换路径：{out_file}")
+
+
+if __name__ == '__main__':
+    c = CoverRadio("../data/cyc.mp3")
+    c.to_wav()
